f0baeac97aeb9627ec0d871ef53ccc699c306dce
Feizza <feizza01@gmail.com>
2022-05-11 13:00:40 -0400

    bullet list

--- RecipeBox/RecipeDetailsViewController.swift
+++ RecipeBox/RecipeDetailsViewController.swift
@@ -12,6 +12,7 @@ class RecipeDetailsViewController: UIViewController {
     var recipeInfo: SearchResult!
     @IBOutlet weak var recipeTitle: UILabel!
     @IBOutlet weak var recipeImage: UIImageView!
+    @IBOutlet weak var recipeIngredients: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         configure()
@@ -23,8 +24,29 @@ class RecipeDetailsViewController: UIViewController {
         if let imageURL = URL(string: recipeInfo.image) {
           downloadTask = recipeImage.loadImage(url: imageURL)
         }
+      //Appending strings to form bullet list: https://bendodson.com/weblog/2018/08/09/bulleted-lists-with-uilabel/#fnref:localise
+       let bullet = "•  "
+       
+       var strings = [String]()
+       strings.append("Payment will be charged to your iTunes account at confirmation of purchase.")
+       strings.append("Your subscription will automatically renew unless auto-renew is turned off at least 24-hours before the end of the current subscription period.")
+       strings.append("Your account will be charged for renewal within 24-hours prior to the end of the current subscription period.")
+       strings.append("Automatic renewals will cost the same price you were originally charged for the subscription.")
+       strings.append("You can manage your subscriptions and turn off auto-renewal by going to your Account Settings on the App Store after purchase.")
+       strings.append("Read our terms of service and privacy policy for more information.")
+       strings = strings.map { return bullet + $0 }
+       
+        var attributes = [NSAttributedString.Key: Any]()
+       attributes[.foregroundColor] = UIColor.darkGray
+       
+       let paragraphStyle = NSMutableParagraphStyle()
+       paragraphStyle.headIndent = (bullet as NSString).size(withAttributes: attributes).width
+       attributes[.paragraphStyle] = paragraphStyle
 
-      }
+       let string = strings.joined(separator: "\n\n")
+       recipeIngredients.attributedText = NSAttributedString(string: string, attributes: attributes)
+    }
+      
 
     /*
     // MARK: - Navigation
