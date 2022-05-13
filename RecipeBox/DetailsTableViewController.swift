4b11c6a6cf7d6433ae5dfa23e944d96d1ca0aed1
Feizza <feizza01@gmail.com>
2022-05-13 12:56:23 -0400

    2 Sections are good to go!!

--- RecipeBox/DetailsTableViewController.swift
+++ RecipeBox/DetailsTableViewController.swift
@@ -31,11 +31,14 @@ class DetailsTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 1
+        return 2
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
+        if section == 0{
+            return 1
+        }
         return quotes.count
     }
 
